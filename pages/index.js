167842bd51e365324af6767f019ef19aa18e7fc7
cassidoo <cassidoo@gmail.com>
2020-05-31 23:57:56 -0700

    Add logic for capturing repos

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,58 @@
-import Head from "next/head";
-import { useState } from "react";
-import RepoField from "../components/RepoField";
+import Head from 'next/head'
+import { useState, useCallback } from 'react'
+
+// https://api.github.com/users/username/repos
+
+function buildURL() {}
 
 export default function Home() {
-  let [numRepos, setNumRepos] = useState(2);
+  let [repos, setRepos] = useState([])
+
+  const onSubmit = useCallback((e) => {
+    e.preventDefault()
+    // let repoA = document.querySelector('input[name="repoA"]')
+    // let repoB = document.querySelector('input[name="repoB"]')
+    // repoA = repoA.value || repoA.placeholder
+    // repoB = repoB.value || repoB.placeholder
+
+    let repoStrings = repos.map((repo) => {
+      return repo.replace(/^https:\/\/github.com\//i, '')
+    })
+
+    console.log(repoStrings)
+
+    let url = repoStrings.map((str) => {
+      return `/${encodeURIComponent(str.replace('/', '#'))}`
+    })
+
+    console.log(`/compare${url.join('')}`)
+
+    // window.location.href = `/compare${url}`
+  })
+
   return (
     <div className="container">
       <Head>
         <title>Sonnet 18 | Repo Comparison</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Add at least 2 GitHub repo URLs:
-      {[...Array(numRepos)].map(() => {
-        return <RepoField />;
-      })}
-      <button
-        onClick={() => {
-          setNumRepos(numRepos + 1);
-        }}
-      >
-        + Add another repo URL
-      </button>
+      Add one repo URL per line, up to 5:
+      <form onSubmit={onSubmit}>
+        <textarea
+          placeholder={`https://github.com/username/test`}
+          defaultValue={repos}
+          onChange={(event) => {
+            let vals = event.target.value.split('\n').filter(function (el) {
+              return el.trim() !== ''
+            })
+
+            setRepos(vals)
+          }}
+        />
+        {repos.length >= 2 && repos.length <= 5 && (
+          <button type="submit">Compare!</button>
+        )}
+      </form>
     </div>
-  );
+  )
 }
