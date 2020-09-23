2fe6029268f311dcc0e924a542cc276bf5f02d31
Cassidy Williams <cassidoo@users.noreply.github.com>
2020-09-23 12:53:51 -0700

    Make more accurate error

--- pages/_error.js
+++ pages/_error.js
@@ -5,8 +5,7 @@ function Error({ statusCode }) {
       {statusCode === 500 ? (
         <div>
           Oopsies. Try refreshing the page! Chances are if you've gotten here,
-          there's an issue with how Next.js and next-on-netlify work together to
-          generate new routes.
+          there's an issue with the GitHub API that limited the response.
         </div>
       ) : (
         `An error occurred.`
